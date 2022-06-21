d02cf568292e1d715bcb931df085999dc93d119f
Carlos William <carloswtmachado@gmail.com>
2022-06-21 18:47:28 -0300

    valida dados recebidos pelo body e headers

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -48,7 +48,45 @@ server.get('/participants', (_, res) => {
 	const promise = db.collection("participantes").find({}).toArray();
 	promise.then(participantes => res.send(participantes));
 	promise.catch(e => console.log(e));
-  });
+});
+
+server.post('/messages', (req, res) => {
+	const { to, text, type } = req.body;
+	const { user } = req.headers;
+	if(!to || !text || !type || !user) return res.status(422).send("Todos os campos sao obrigatorios");
+	if(type !== 'message' && type !== 'private_message') return res.status(422).send("type deve ser 'message' ou 'private_message'");
+	const promise_find = db.collection("participantes").findOne({name: user});
+	promise_find.then(participante => {
+		if(!participante) return res.status(422).send("usuario nao cadastrado");
+		else return res.sendStatus(200);
+	});
+	//res.send(user);
+	/*
+	if(!name) return res.sendStatus(422);
+	const promise_find = db.collection("participantes").findOne({name: name});
+	promise_find.then(participante => {
+		if(!participante){
+			const promise_inserir = db.collection("participantes").insertOne({
+				name: name, 
+				lastStatus: Date.now()
+			});
+			promise_inserir.then(() => {
+				const promise_mensagem = db.collection("mensagens").insertOne({
+					from: name,
+					to: 'Todos', 
+					text: 'entra na sala...', 
+					type: 'status', 
+					time: dayjs().format('HH:mm:ss')
+				});
+				promise_mensagem.then(() => res.sendStatus(201));
+				promise_mensagem.catch(e => console.log("deu erro pra inserir a mensagem"));
+			});
+			promise_inserir.catch(e => console.log("deu erro pra inserir o participante"));
+		}
+		else return res.sendStatus(409);
+	});
+	*/
+});
 
 server.listen(5000, () => {
 	console.log("Rodando em http://localhost:5000");
